dcf44b177273d5c525757b786b5bc810388b09c8
amirbekataxanov-commits <amirbekataxanov@gmail.com>
2026-03-17 10:38:32 +0500

    Delete const.py

--- const.py
+++
@@ -1,2 +0,0 @@
-ERROR_MSG = "Nimadir xato ketdi, iltimos qaytadan urunib ko'ring."
-LIMIT = "Siz kunlik limitni ishlatib bo'lgansiz, botdan cheksiz foydalanish uchun premium sotib olishingiz kerak."
